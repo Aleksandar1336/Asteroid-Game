9f73d9ff1c9f808b03247a0cbfb0b61631c546de
Aleksandar1336 <aleksandarhristov943@gmail.com>
2026-02-06 00:21:34 +0200

    Add Player Render To Screen

--- main.py
+++ main.py
@@ -1,20 +1,31 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
-	
+from player import Player
+
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
-    	log_state()
-    	for event in pygame.event.get():
-    		if event.type == pygame.QUIT:
-        		return
-    	pygame.Surface.fill(screen, "black")
-    	pygame.display.flip()
+        log_state()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        pygame.Surface.fill(screen, "black")
+        player.draw(screen)
+        pygame.display.flip()
+        dt = pygame.time.Clock.tick(clock, 60) / 1000
+
+    print(dt)
     print(f"Starting Asteroids...")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+
 if __name__ == "__main__":
     main()
+
