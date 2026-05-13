10cc65697c4a6e0de0faa43914813d2c2ec9dbb3
Aleksandar1336 <aleksandarhristov943@gmail.com>
2026-05-13 12:39:34 +0300

    player moves left and right

--- player.py
+++ player.py
@@ -1,6 +1,6 @@
 
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, LINE_WIDTH
+from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED
 import pygame
 
 class Player(CircleShape):
@@ -24,5 +24,13 @@ class Player(CircleShape):
         b = self.position - forward * self.radius - right
         c = self.position - forward * self.radius + right
         return [a, b, c]
-     
- 
+      def rotate(self, dt):
+         self.rotation += PLAYER_TURN_SPEED * dt
+      def update(self, dt):
+         keys = pygame.key.get_pressed()
+
+         if keys[pygame.K_d]:   # rotate right
+             self.rotate(dt)
+
+         if keys[pygame.K_a]:   # rotate left
+            self.rotate(-dt)
